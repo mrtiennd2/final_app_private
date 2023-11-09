6465488c6f72ae0bab9c990c051d33937f002528
mrtiennd2 <tiennd2@nustechnology.com>
2023-11-09 14:49:59 +0700

    feat: updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # attr_accessible :first_name, :last_name
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :albums, dependent: :destroy
   has_many :photos, dependent: :destroy
+
+  # validates :first_name, presence: true, length: { maximum: 25 }
+  # validates :last_name, presence: true, length: { maximum: 25 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { maximum: 64 }
 end
