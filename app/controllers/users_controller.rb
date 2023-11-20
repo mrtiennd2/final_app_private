2ada6f290cebc19a838ca543b53e3cbb04aa0a7a
mrtiennd2 <tiennd2@nustechnology.com>
2023-11-20 10:13:19 +0700

    feat: add following logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  layout 'profile_layout', only: %i[show albums photos]
+  layout 'profile_layout'
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :set_user, except: %i[index]
 
   def index; end
@@ -17,7 +17,18 @@ class UsersController < ApplicationController
   end
 
   def follow
-    # ...
+    @follower = Follower.where(user_id: @user.id, follower_id: current_user.id).first_or_initialize
+    user_fname = full_name(@user)
+    if !@follower.persisted?
+      if @follower.save
+        redirect_to @user, notice: "Now following #{user_fname}"
+      else
+        redirect_to @user, notice: "Failed to follow #{user_fname}"
+      end
+    else
+      @follower.destroy
+      redirect_to @user, notice: "Unfollow #{user_fname}"
+    end
   end
 
   def unfollow
@@ -25,11 +36,11 @@ class UsersController < ApplicationController
   end
 
   def followers
-    @followers = @user.follwers
+    @followers = @user.followers
   end
 
   def followings
-    @following = @user.followings
+    @followings = @user.followings
   end
 
   private
@@ -39,7 +50,13 @@ class UsersController < ApplicationController
   end
 
   def set_user
-    user_id = params[:id] || params[:user_id]
+    user_id = params[:id]
     @user = User.includes(:photos, :albums).find(user_id)
+  end
+
+  def full_name(user)
+    fst_char = user.first_name || ' '
+    snd_char = user.last_name || ' '
+    "#{fst_char} #{snd_char}"
   end
 end
