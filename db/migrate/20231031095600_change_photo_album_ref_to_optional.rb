6fa5a68468cecb449f43a906fe6348c296238fd5
mrtiennd2 <tiennd2@nustechnology.com>
2023-11-21 16:48:57 +0700

    feat: change migrations (change_column) to be reversible

--- db/migrate/20231031095600_change_photo_album_ref_to_optional.rb
+++ db/migrate/20231031095600_change_photo_album_ref_to_optional.rb
@@ -1,5 +1,13 @@
 class ChangePhotoAlbumRefToOptional < ActiveRecord::Migration[7.1]
   def change
-    change_column :photos, :album_id, :bigint, null: true
+    change_column_null(:photos, :album_id, false)
   end
+
+  # def down
+  #   change_column :photos, :album_id, :bigint, null: false
+  # end
+  #
+  # def up
+  #   change_column :photos, :album_id, :bigint, null: true
+  # end
 end
--- db/migrate/20231116090823_change_user_id_null_true.rb
+++ db/migrate/20231116090823_change_user_id_null_true.rb
@@ -1,5 +1,13 @@
 class ChangeUserIdNullTrue < ActiveRecord::Migration[7.1]
   def change
-    change_column :likes, :user_id, :integer, null: true
+    change_column_null(:likes, :user_id, true)
   end
+
+  # def down
+  #   change_column :likes, :user_id, :integer, null: false
+  # end
+  #
+  # def up
+  #   change_column :likes, :user_id, :integer, null: true
+  # end
 end
