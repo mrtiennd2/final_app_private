273f821a959a0d7154a2389805d26c44789d4273
TengNg <nguyentin148@gmail.com>
2023-11-22 09:28:35 +0700

    feat: multiple-select in album's photos

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -14,7 +14,7 @@ class AlbumsController < ApplicationController
       if current_user.is_admin
         Album.all.page(params[:page])
       else
-        current_user.albums.page(params[:page])
+        current_user.albums.page(params[:page]).per(10)
       end
   end
 
@@ -39,26 +39,20 @@ class AlbumsController < ApplicationController
 
   def create
     @album = current_user.albums.build(album_params)
-
-    if params[:album][:photo_image].present?
-      @album.photos_attributes = [{ user_id: current_user.id, image_url: params[:album][:photo_image] }]
-    end
+    @album.photos_attributes = attached_photos
 
     if @album.save
-      redirect_to edit_album_url(@album), notice: 'Album was successfully created.'
+      redirect_to edit_album_path(@album), notice: 'Album was successfully created.'
     else
       render new_album_path, status: :unprocessable_entity, notice: @album.errors.full_messages
     end
   end
 
   def update
-    if params[:album][:photo_image].present?
-      @album.photos_attributes = [{ user_id: current_user.id, image_url: params[:album][:photo_image] }]
-    end
-
+    @album.photos_attributes = attached_photos
     respond_to do |format|
       if @album.update(album_params)
-        format.html { redirect_to edit_album_url(@album), notice: 'Album was successfully updated.' }
+        format.html { redirect_to edit_album_path(@album), notice: 'Album was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity, notice: 'Failed to save album' }
       end
@@ -69,7 +63,7 @@ class AlbumsController < ApplicationController
     @album.destroy!
 
     respond_to do |format|
-      format.html { redirect_to albums_url, notice: 'Album was successfully destroyed.' }
+      format.html { redirect_to albums_path, notice: 'Album was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -78,6 +72,19 @@ class AlbumsController < ApplicationController
 
   def set_album
     @album = Album.find(params[:id] || params[:album_id])
+  end
+
+  def attached_photos
+    if params[:album][:photo_images].present?
+      images = []
+      params[:album][:photo_images].each do |image|
+        next if image.blank?
+
+        item = { user_id: current_user.id, image_url: image }
+        images << item
+      end
+    end
+    images
   end
 
   def album_params
